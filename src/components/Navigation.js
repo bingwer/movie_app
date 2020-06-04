cefaa305168de0db78bfe2261feadea05478eef5
Seunghyo Lee <bingwer@naver.com>
2020-06-04 16:43:10 +0900

    add navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <div className="nav">
+      <Link to="/" replace>
+        Home
+      </Link>
+      <Link to="/about" replace>
+        About
+      </Link>
+    </div>
+  );
+}
+
+export default Navigation;
