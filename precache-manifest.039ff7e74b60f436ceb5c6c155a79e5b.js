c97c932471a407b427b11231c9387e4f5194ae10
Seunghyo Lee <bingwer@naver.com>
2020-06-04 17:39:07 +0900

    Updates

---
+++ precache-manifest.039ff7e74b60f436ceb5c6c155a79e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a781ab4541fab9e3baabb294c20670f3",
+    "url": "/bingwer/movie_app/index.html"
+  },
+  {
+    "revision": "61ba86d0f3a0559bcb26",
+    "url": "/bingwer/movie_app/static/css/main.3983e3e2.chunk.css"
+  },
+  {
+    "revision": "79483aab3304f8775d7f",
+    "url": "/bingwer/movie_app/static/js/2.cda10918.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bingwer/movie_app/static/js/2.cda10918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ba86d0f3a0559bcb26",
+    "url": "/bingwer/movie_app/static/js/main.792d6e9d.chunk.js"
+  },
+  {
+    "revision": "d31f0653971d2d068180",
+    "url": "/bingwer/movie_app/static/js/runtime-main.e3accfde.js"
+  }
+]);
